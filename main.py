720e6db3968b3aec2c33209b718e3d24156b6138
Sergey Chernyshov <thesaren117@yandex.ru>
2019-11-19 20:34:48 +0300

    Исправление логики приложения

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 try:
     user_input = input('Введите  выражение в польской нотации через пробелы:')
-    assert user_input[0] == '+' or user_input[0] == '-' or user_input[0] == '*' or user_input[0] == '/','Используем только +-*/'
+    assert user_input[0] in ('+', '-', '*', '/'), 'Используем только +-*/'
 except AssertionError:
     print('Используем только +-*/ и не забываем ставить пробел')
 
 input_list = user_input.split(' ')
-print(input_list)
+# print(input_list)
 sign = input_list[0]
 try:
     first_number = int(input_list[1])
     second_number = int(input_list[2])
 except IndexError:
     print('Не введены все условия!')
+except ValueError:
+    print('Условия должны быть в ввиде числа.')
+
 try:
     if sign == '+':
         print(first_number + second_number)
@@ -22,6 +25,9 @@ try:
     elif sign == '/':
          print(first_number // second_number)
     else:
-        print('Не забываем ставить пробел')
+        class MyException(Exception):
+            print('Не забываем ставить пробелы!')
 except ZeroDivisionError:
     print('На ноль делить нельзя!')
+except NameError:
+    print('Условия должны быть в ввиде числа.')
